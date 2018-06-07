bcd2600e9afb010ef228dfecf658b4d21f999975
JDumadag25 <justin.dumadag@gmail.com>
2018-06-07 12:04:58 -0400

    got music to play in app to be refactored

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import { BrowserRouter as Router, Route, NavLink, Redirect, Switch } from 'react
 class App extends Component {
 
   state= {
-    errors: []
+    errors: [],
+    
   }
 
   login = (username, password, callback) => {
@@ -34,16 +35,45 @@ class App extends Component {
       }
       });
   }
+
+  register = (username, password, callback) => {
+    fetch('http://localhost:3000/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({ username, password })})
+      .then(res => res.json())
+      .then(json => {
+        if(json.token){
+        localStorage.setItem('token', json.token);
+        localStorage.setItem('user_id', json.user_id);
+        localStorage.setItem('username', json.username);
+
+        callback("/partyroom");
+      } else {
+        console.log(json.errors);
+        this.setState({errors:[json.errors]})
+      }
+      });
+  }
+
+    // TODO: Add a logout button somewhere and call this.
+  logout = () => {
+    // You'll want to use:
+    //   localStorage.removeItem('key')
+  }
   render() {
 
     return (
       <Router>
         <Switch>
-          <Route exact path="/beef" component={Home} />
+          <Route exact path="/" component={Home} />
 
           <Route path="/login" render={(props) => <Login submitLabel="Login" onSubmit={this.login} {...props} errors={this.state.errors} />} />
 
-          <Route path="/register" render={(props) => <Register submitLabel="Register" onSubmit={this.register} {...props} />} />
+          <Route path="/register" render={(props) => <Register submitLabel="Register" onSubmit={this.register} {...props} errors={this.state.errors}/>} />
 
           { localStorage.getItem('token') ? <Route path="/partyroom" render={(props) => <Partify {...props} />} /> : <Redirect to="/login" /> }
         </Switch>
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,10 +3,30 @@ import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui
 
 class Register extends React.Component {
   state = {
-
+    username:'',
+    password:'',
+    passwordConfirmation:'',
+    errors:[]
   }
 
+  handleChange = (event) => {
+  this.setState({
+    [event.target.name]: event.target.value
+  })
+ }
+
+ handleSubmit = (event) => {
+   event.preventDefault();
+   console.log(this.props);
+   if (this.state.password === this.state.passwordConfirmation){
+   this.props.onSubmit(this.state.username, this.state.password, this.props.history.push)
+ } else {
+   this.setState({errors: ['Passwords do not match']})
+ }
+ }
+
   render(){
+    const errors = this.props.errors.map(error => <h2>Username and Password cannot be blank</h2>)
     return(
       <div className='login-form'>
         <style>{`
@@ -21,15 +41,19 @@ class Register extends React.Component {
             <Header as='h2' color='grey' textAlign='center'>
               Sign up
             </Header>
+            <p>{errors}</p>
+            <h3>{this.state.errors}</h3>
             <Form size='large' onSubmit={this.handleSubmit}>
               <Segment stacked>
-                <Form.Input fluid icon='user' iconPosition='left' placeholder='Username' onChange={this.handle} />
+                <Form.Input fluid icon='user' iconPosition='left' placeholder='Username' name='username' onChange={this.handleChange} />
                 <Form.Input
                   fluid
                   icon='lock'
                   iconPosition='left'
                   placeholder='Password'
                   type='password'
+                  name='password'
+                  onChange={this.handleChange}
                 />
                 <Form.Input
                   fluid
@@ -37,6 +61,8 @@ class Register extends React.Component {
                   iconPosition='left'
                   placeholder='Confirm Password'
                   type='password'
+                  name='passwordConfirmation'
+                  onChange={this.handleChange}
                 />
 
               <Button color='grey' fluid size='large'>
